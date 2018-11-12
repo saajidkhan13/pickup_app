f6eebc0ed9c9218af9fa67b8b137197de087cadd
github username <github email address>
2018-11-12 17:37:59 -0500

    all sorts of stuff like events controllers views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,43 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+    redirect_to @event
+  end
+
+  def edit
+
+  end
+
+  def update
+    @event.update(event_params)
+    redirect_to @event
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def find_event
+    @event = Event.find_by(id: params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:sport_id, :title, :date, :time, :location, :duration)
+  end
 end
