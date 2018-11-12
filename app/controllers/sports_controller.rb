6dc8eeab4c360f4a1b82a59cb381cea69fe8dd0f
github username <github email address>
2018-11-12 15:29:46 -0500

    first try at this

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,2 +1,40 @@
 class SportsController < ApplicationController
+  before_action :find_sport, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @sports = Sport.all
+  end
+
+  def show
+  end
+
+  def method
+    #code
+  end
+
+  def method
+    #code
+  end
+
+  def method
+    #code
+  end
+
+  def method
+    #code
+  end
+
+  def method
+    #code
+  end
+
+  private
+
+  def find_sport
+    @sport = Sport.find_by(id: params[:id])
+  end
+
+  def sport_params
+    params.require(:sport).permit(:name, :player_count)
+  end
 end
