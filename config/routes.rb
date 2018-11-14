01346690bb026af0c6c9ff00ca134b319e4440c3
tirempbs <tirem@fastmail.com>
2018-11-14 15:19:38 -0500

    added a splash page, controller, and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   resources :events
   resources :users
   resources :sports
+
+  get "/", to: "welcome#home", as: "landing"
   get "signup", to: "users#new", as: "signup"
   get "login", to: "sessions#new", as: "login"
   post "login", to: "sessions#create"
-  # post "sessions", to: "sessions#create", as: "sessions"
   delete "logout", to: "sessions#destroy", as: "logout"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
